9bf0f2d83931e5eeaa23ff3ee2446be9f59e9c70
tiqwell <98182406+tiqwell@users.noreply.github.com>
2024-05-11 12:20:34 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+rows = int(input("Введите количество строк: "))
+cols = int(input("Введите количество столбцов: "))
+
+array = []
+
+print("введите массив:")
+
+for i in range(rows):
+    row = list(input())
+    array.append(row)
+
+startX = int(input("Введите стартову позицию X: "))
+startY = int(input("Введите стартову позицию Y: "))
+
+
